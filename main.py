4b641cf21cd43b4590a6842a947e3f87085bcc7d
sovichea-sinath <sinathsovichea@gmail.com>
2022-07-23 22:12:33 +0700

    fix the parser test

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import unittest
 from lexer import lex
 
-def main():
-  assert lex('{"foo": [1, 2, {"bar": 2}]}') == ['{', 'foo', ':', '[', 1, ',', 2, ',', '{', 'bar', ':', 2, '}', ']', '}']
+
+class TestStringMethods(unittest.TestCase):
+  def test_parser(self):
+    tokens = lex('{"foo": [1, 2, {"bar": 2}]}')
+    unittest.TestCase.assertEquals(self, tokens, ['{', 'foo', ':', '[', 1, ',', 2, ',', '{', 'bar', ':', 2, '}', ']', '}'])
+
 
 if __name__ == "__main__":
-  main()
+  unittest.main()
